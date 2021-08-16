b73eb7227bb91b0e63b1a9dc015d2201868b1c35
Alan He <qianghe421@gmail.com>
2021-08-16 19:27:10 +0800

    fix(filter): query be undefined

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,47 +1,47 @@
-import {ScriptFilter, ScriptFilterItem} from "./interface";
+import { ScriptFilter, ScriptFilterItem } from './interface';
 
 const SPLIT_TOKEN = '✩';
 const utils = {
-    /**
-     * 根据params列出的属性进行过滤，不区分大小写
-     * 如果查询关键词为空，返回原数组
-     */
-    filterItemsBy: (items: ScriptFilterItem[], query: string, ...params: ('title' | 'subtitle')[]) => {
-        query = query.trim();
-        if (query) {
-            return items.filter((item) =>
-                params.some((p) => item[p] && item[p].match(new RegExp(query, 'i')))
-            );
-        } else {
-            return items;
-        }
-    },
-
-    quickLookUrl4File: (filename: string) => `file://${filename.replace(/\s/g, '%20')}`,
-
-    /**
-     * 过滤列表
-     * @param sf
-     */
-    outputScriptFilter: (sf: ScriptFilter) => {
-        console.log(JSON.stringify(sf));
-    },
-
-    /**
-     * Script item arg为字符串，有时需要多个参数传递，可以使用字符串拼接
-     */
-    joinMultiArg: (...args: (number | string | boolean) []) => args.join(SPLIT_TOKEN),
-
-    splitMultiArgStr: (argStr: string): string[] => argStr.split(SPLIT_TOKEN),
-
-    /**
-     * Mac自带emoji表情支持
-     * @see https://support.apple.com/zh-cn/guide/mac-help/mchlp1560/mac
-     */
-    emoji: {
-        checked: '☑️',
-        unchecked: '✖️'
+  /**
+   * 根据params列出的属性进行过滤，不区分大小写
+   * 如果查询关键词为空，返回原数组
+   */
+  filterItemsBy: (items: ScriptFilterItem[], query = '', ...params: ('title' | 'subtitle')[]) => {
+    query = query.trim();
+    if (query) {
+      return items.filter((item) =>
+        params.some((p) => item[p] && item[p].match(new RegExp(query, 'i')))
+      );
+    } else {
+      return items;
     }
+  },
+
+  quickLookUrl4File: (filename: string) => `file://${filename.replace(/\s/g, '%20')}`,
+
+  /**
+   * 过滤列表
+   * @param sf
+   */
+  outputScriptFilter: (sf: ScriptFilter) => {
+    console.log(JSON.stringify(sf));
+  },
+
+  /**
+   * Script item arg为字符串，有时需要多个参数传递，可以使用字符串拼接
+   */
+  joinMultiArg: (...args: (number | string | boolean) []) => args.join(SPLIT_TOKEN),
+
+  splitMultiArgStr: (argStr: string): string[] => argStr.split(SPLIT_TOKEN),
+
+  /**
+   * Mac自带emoji表情支持
+   * @see https://support.apple.com/zh-cn/guide/mac-help/mchlp1560/mac
+   */
+  emoji: {
+    checked: '☑️',
+    unchecked: '✖️'
+  }
 };
 
 export default utils;
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,49 +1,62 @@
-import {utils} from "../lib";
+import { utils } from '../lib';
 
 test('filterItemsBy is ok', () => {
-    // title命中
-    expect(utils.filterItemsBy([{
-        title: 'hello',
-        subtitle: ''
-    }], 'hel', 'title')).toStrictEqual([
-        {
-            title: 'hello',
-            subtitle: ''
-        }
-    ]);
-    // subtitle命中
-    expect(utils.filterItemsBy([{
-        title: 'zxxx',
-        subtitle: 'hello'
-    }], 'hel', 'title', 'subtitle')).toStrictEqual([
-        {
-            title: 'zxxx',
-            subtitle: 'hello'
-        }
-    ]);
+  // title命中
+  expect(utils.filterItemsBy([{
+    title: 'hello',
+    subtitle: ''
+  }], 'hel', 'title')).toStrictEqual([
+    {
+      title: 'hello',
+      subtitle: ''
+    }
+  ]);
+  // subtitle命中
+  expect(utils.filterItemsBy([{
+    title: 'zxxx',
+    subtitle: 'hello'
+  }], 'hel', 'title', 'subtitle')).toStrictEqual([
+    {
+      title: 'zxxx',
+      subtitle: 'hello'
+    }
+  ]);
 
-    // 关键词为空
-    expect(utils.filterItemsBy([{
-        title: 'zxxx',
-        subtitle: 'hello'
-    }], '', 'title', 'subtitle')).toStrictEqual([
-        {
-            title: 'zxxx',
-            subtitle: 'hello'
-        }
-    ]);
+  // 关键词为空
+  expect(utils.filterItemsBy([{
+    title: 'zxxx',
+    subtitle: 'hello'
+  }], '', 'title', 'subtitle')).toStrictEqual([
+    {
+      title: 'zxxx',
+      subtitle: 'hello'
+    }
+  ]);
 
-    // 关键词首尾空格
-    expect(utils.filterItemsBy([{
-        title: 'hello',
-        subtitle: ''
-    }], ' hel ', 'title')).toStrictEqual([
-        {
-            title: 'hello',
-            subtitle: ''
-        }
-    ]);
+  // 关键词首尾空格
+  expect(utils.filterItemsBy([{
+    title: 'hello',
+    subtitle: ''
+  }], ' hel ', 'title')).toStrictEqual([
+    {
+      title: 'hello',
+      subtitle: ''
+    }
+  ]);
 
-    // 参数合并
-    expect(utils.joinMultiArg(1, true, 'hello')).toStrictEqual('1✩true✩hello');
+  // 参数合并
+  expect(utils.joinMultiArg(1, true, 'hello')).toStrictEqual('1✩true✩hello');
 });
+
+test('filterItemsBy query is undefined', () => {
+  expect(utils.filterItemsBy([{
+    title: 'hello',
+    subtitle: ''
+  }], undefined, 'title')).toStrictEqual([
+    {
+      title: 'hello',
+      subtitle: ''
+    }
+  ]);
+});
+
