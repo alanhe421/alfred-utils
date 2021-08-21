e256791d2e500e7661457de685bede2373d134d2
Alan He <qianghe421@gmail.com>
2021-08-21 22:36:41 +0800

    fix: 下载实现改为函数，不自动执行

--- lib/alfred-cli-download.ts
+++ lib/alfred-cli-download.ts
@@ -1,5 +1,9 @@
 import {WorkflowUpdater} from "./workflow-updater";
 
-const updater = new WorkflowUpdater(process.env.remote_info_list_url,
-    process.env.remote_workflow_url, process.env.alfred_workflow_version);
-updater.download();
+export function download() {
+    const updater = new WorkflowUpdater(process.env.remote_info_list_url,
+        process.env.remote_workflow_url, process.env.alfred_workflow_version);
+    updater.download();
+}
+
+
