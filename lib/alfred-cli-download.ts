4e4300efc5bc55984cc399c7eef85084c441910e
Alan He <qianghe421@gmail.com>
2021-08-22 17:30:36 +0800

    Revert "fix: 下载实现改为函数，不自动执行"
    This reverts commit e256791d2e500e7661457de685bede2373d134d2.

--- lib/alfred-cli-download.ts
+++ lib/alfred-cli-download.ts
@@ -1,9 +1,5 @@
 import {WorkflowUpdater} from "./workflow-updater";
 
-export function download() {
-    const updater = new WorkflowUpdater(process.env.remote_info_list_url,
-        process.env.remote_workflow_url, process.env.alfred_workflow_version);
-    updater.download();
-}
-
-
+const updater = new WorkflowUpdater(process.env.remote_info_list_url,
+    process.env.remote_workflow_url, process.env.alfred_workflow_version);
+updater.download();
