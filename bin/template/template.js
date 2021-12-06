48f54e4b0b15ffa815b01bb6d6dbf6944434e586
alanhe <qianghe421@gmail.com>
2021-12-07 00:02:06 +0800

    feat: add main func

--- bin/template/template.js
+++ bin/template/template.js
@@ -1,7 +1,18 @@
 /**
  * usage
- * /usr/local/bin/node ./__filename__.js
+ * /usr/local/bin/node ./__filename__.js {query}
  */
 const {utils, http} = require('@stacker/alfred-utils');
 const [, ,] = process.argv;
 const instance = http.createHttpClient();
+
+(function () {
+  utils.outputScriptFilter({
+    items: [
+      utils.buildItem({
+        title: 'hello',
+        subtitle: 'easy to use alfred-utils'
+      })
+    ]
+  })
+})();
