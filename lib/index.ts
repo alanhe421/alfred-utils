544f9b0aaa93f69e6eea5f5c58b2bf8c9eca3ee1
alanhe <qianghe421@gmail.com>
2022-01-15 23:00:57 +0800

    fix: 支持导出命令参数

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,8 @@
-export {default as http} from './http';
-export {default as utils} from './utils';
-export {default as dateUtils} from './date-utils';
+export { default as http } from './http';
+export { default as utils } from './utils';
+export { default as dateUtils } from './date-utils';
+
+/**
+ * @description 读取命令参数
+ */
+export const input = process.argv[2];
