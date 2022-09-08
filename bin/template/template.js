79bb3159056098a9da44ba9970e6ce6d15cf5bd4
alanhe <qianghe421@gmail.com>
2022-09-08 22:02:46 +0800

    fix(template): update template

--- bin/template/template.js
+++ bin/template/template.js
@@ -4,17 +4,16 @@
  * usage
  * /usr/local/bin/node ./__filename__.js {query}
  */
-const {utils, http} = require('@stacker/alfred-utils');
+const {http, Workflow} = require('@stacker/alfred-utils');
 const [, ,] = process.argv;
 const instance = http.createHttpClient();
-
+const wf = new Workflow();
 (function () {
-  utils.printScriptFilter({
-    items: [
-      utils.buildItem({
-        title: 'hello',
-        subtitle: 'easy to use alfred-utils'
-      })
-    ]
-  })
+  wf.addWorkflowItem({
+    item: {
+      title: 'hello', subtitle: 'easy to use alfred-utils'
+    }
+  });
+
+  wf.run();
 })();
