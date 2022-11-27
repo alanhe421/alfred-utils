772d88954e5ec6b3771373b3fe7dff44d7f733ab
Alan He <qianghe421@gmail.com>
2022-11-28 00:46:15 +0800

    closed #26

--- lib/index.ts
+++ lib/index.ts
@@ -6,5 +6,8 @@ export { WorkflowUpdater } from './workflow-updater';
 
 /**
  * @description 读取命令参数
+ * @deprecated 推荐使用inputParams
  */
 export const input = process.argv[2];
+
+export const inputParams = process.argv.slice(2);
